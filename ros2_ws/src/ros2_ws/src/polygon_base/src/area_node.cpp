f78e3cc314b0788416f7f145bb663c19c37582c6
mczx <matheucheo16@gmail.com>
2026-05-15 10:35:49 +0800

    Confirmed clean build: urdf_tutorial_cpp and remaining packages successful

---
+++ ros2_ws/src/ros2_ws/src/polygon_base/src/area_node.cpp
@@ -0,0 +1,29 @@
+#include <pluginlib/class_loader.hpp>
+#include <polygon_base/regular_polygon.hpp>
+
+int main(int argc, char** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  pluginlib::ClassLoader<polygon_base::RegularPolygon> poly_loader("polygon_base", "polygon_base::RegularPolygon");
+
+  try
+  {
+    // Note: Check if your plugin name is "polygon_plugins::Triangle" or "awesome_triangle" in plugins.xml
+    std::shared_ptr<polygon_base::RegularPolygon> triangle = poly_loader.createSharedInstance("polygon_plugins::Triangle");
+    triangle->initialize(10.0);
+
+    std::shared_ptr<polygon_base::RegularPolygon> square = poly_loader.createSharedInstance("polygon_plugins::Square");
+    square->initialize(10.0);
+
+    printf("Triangle area: %.2f\n", triangle->area());
+    printf("Square area: %.2f\n", square->area());
+  }
+  catch(pluginlib::PluginlibException& ex)
+  {
+    printf("The plugin failed to load. Error: %s\n", ex.what());
+  }
+
+  return 0;
+}
